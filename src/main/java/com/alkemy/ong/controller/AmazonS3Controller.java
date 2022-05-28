7f9b14ea527716401f4fe978b10a0a5d00731afd
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-27 22:04:03 -0300

    Unnecesary files deleted

--- src/main/java/com/alkemy/ong/controller/AmazonS3Controller.java
+++ src/main/java/com/alkemy/ong/controller/AmazonS3Controller.java
@@ -1,9 +1,6 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.service.AmazonS3Service;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -18,17 +18,11 @@ import java.util.List;
 @Api(tags = "Users")
 public class  UserController {
 
-
     private final IUserService userService;
-
-
 
     @GetMapping
     public ResponseEntity<List<UserBasicDto>> getAllUsers(){
-
-        //TODO - When JWT is configured, i am going to modify that only admin users can use this endpoint
         List<UserBasicDto> userList = userService.returnList();
-
         return ResponseEntity.status(HttpStatus.OK).body(userList);
     }
 
@@ -42,5 +36,4 @@ public class  UserController {
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
